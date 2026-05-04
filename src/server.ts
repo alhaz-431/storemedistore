47d66f49b563b3bc06b9f9cf966d8dc1213eb2d6
Alhaz <alhaz@gmail.com>
2026-05-04 21:51:31 +0600

    updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,23 @@ import app from "./app";
 
 const PORT = process.env.PORT || 5000;
 
-
-app.get('/', (req, res) => {
-  res.send('MediStore API is running perfectly!');
+// uncaught exception (must)
+process.on("uncaughtException", (err: any) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.error(err);
+  process.exit(1);
 });
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+
+// server start
+const server = app.listen(PORT, () => {
+  console.log(`🚀 MediStore Server is running on port ${PORT}`);
+});
+
+// unhandled rejection
+process.on("unhandledRejection", (err: any) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.error(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
