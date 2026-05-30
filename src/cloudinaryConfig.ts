ad350dddbdb84a3c2ac30f17c877995a00f6cb08
Alhaz <alhaz@gmail.com>
2026-05-30 16:49:28 +0600

    updated cloudinary page

--- src/cloudinaryConfig.ts
+++ src/cloudinaryConfig.ts
@@ -2,10 +2,11 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 
+// এখানে তোমার আসল ভ্যালুগুলো বসিয়ে দিয়েছি
 cloudinary.config({
   cloud_name: 'da55p8fpm',
-  api_key: 'আপনার_API_KEY_এখানে', // Cloudinary থেকে কপি করে দিন
-  api_secret: 'আপনার_API_SECRET_এখানে' // Cloudinary থেকে কপি করে দিন
+  api_key: '563446485239195',
+  api_secret: 'o9zkb5OUqaNeVAnn3f9b5VYGvJo'
 });
 
 const storage = new CloudinaryStorage({
